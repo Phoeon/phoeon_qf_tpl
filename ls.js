7b4762e02de5b7c91f87d3b61b00875fd5d93e17
fuyue <fuyue@beisen.com>
2016-01-16 16:59:10 +0800

    init project

---
+++ ls.js
@@ -0,0 +1,49 @@
+(function(){
+	debugger
+	var pre = document.querySelector("pre"),
+		title = document.title+"_"+document.URL.match(/\d{8}/)[0],
+		data = localStorage.getItem(title) ;
+
+	data?(pre.innerHTML=data):localStorage.setItem(title,pre.innerHTML);
+	
+	document.addEventListener("keydown",function(e){
+		if( e.ctrlKey  == true && e.keyCode == 83 ){
+			localStorage.setItem(title,pre.innerHTML);
+			e.preventDefault();
+			return false;
+		}
+		return false;
+	},false);
+	document.addEventListener("click",function(e){
+		var target = e.target;
+		if(target.tagName.toLowerCase()==="label"&&target.className==="toggle"){
+			var sib = target.nextElementSibling||{};
+			if(/hidden/.test(sib.className)){
+				sib.className = sib.className.replace(/\s*hidden/,"");
+			}else {
+				sib.className += " hidden" ;
+			}
+		}
+	},false);
+	pre.addEventListener("dblclick",function(e){
+		var target = pre,
+		    html = target.innerHTML;
+		    if(target.contentEditable===true)return false;
+		  	else target.contentEditable = true ;
+		target.innerHTML = html.replace(/[<>]/g,function(c){
+			return c==="<"?"&lt;":"&gt;";
+		});
+		target.focus();
+	},true);
+
+	pre.addEventListener("blur",function(e){
+		var target = pre,
+		    html = target.innerHTML;
+			target.contentEditable = false;
+		target.innerHTML = html.replace(/(&lt;|&gt;)/g,function(c){
+			return c==="&lt;"?"<":">";
+		});
+		localStorage.setItem(title,target.innerHTML);
+	},false);
+
+})()
